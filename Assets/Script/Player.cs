5e3834d4b71f902002109a7d6718e9a26996c290
unknown <r.sulfus2@gmail.com>
2023-06-04 21:07:24 +0330

    android control added + menu added

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -9,6 +9,8 @@ public class Player : MonoBehaviour
     AudioSource _AudioPlayer;
     public AudioClip JumpingSound;
     public float forcejump=4;
+    public GameObject menu;
+    bool goRight,goLeft;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +22,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Running
+        //Runningforpc
         if(Input.GetKey(KeyCode.D)){
             transform.Translate(new Vector2(2f*Time.deltaTime,0));
             transform.localScale=new Vector3(1,transform.localScale.y,transform.localScale.z);
@@ -31,20 +33,57 @@ public class Player : MonoBehaviour
             transform.localScale=new Vector3(-1,transform.localScale.y,transform.localScale.z);
             MyAnim.SetBool(name: "Running",true);
         }
+        //Running for android 
+        if(goRight){
+            transform.Translate(new Vector2(2f*Time.deltaTime,0));
+            transform.localScale=new Vector3(1,transform.localScale.y,transform.localScale.z);
+            MyAnim.SetBool("Running",true);
+        }
+        if(goLeft){
+            transform.Translate(new Vector2(-2f*Time.deltaTime,0));
+            transform.localScale=new Vector3(-1,transform.localScale.y,transform.localScale.z);
+            MyAnim.SetBool(name: "Running",true);
+        }
         //not Running : music ===> mute
-        if(!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A)){
+        if(!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A)&& goLeft==false && goRight==false){
 
            
             MyAnim.SetBool("Running",false);
         }
-        //Jumping
+        //Jumping for pc 
         if(Input.GetKeyDown(KeyCode.Space)){
+            
             charjump.velocity=new Vector2 (charjump.velocity.x,forcejump);
             MyAnim.Play("Jump_Animation");
             _AudioPlayer.PlayOneShot(JumpingSound);
                         MyAnim.SetBool("Running",false);
 
         }
+        //player falling ==> game over
+        if(transform.position.y<-4.5){
+            menu.SetActive(true);
+        }
         
     }
+    public void click_down_Right(){
+        goRight=true;
+    }
+        public void click_up_Right(){
+        goRight=false;
+    }
+     public void click_down_Left(){
+        goLeft=true;
+    }
+        public void click_up_Left(){
+        goLeft=false;
+    }
+    //juping for android 
+        public void jump(){
+             charjump.velocity=new Vector2 (charjump.velocity.x,forcejump);
+            MyAnim.Play("Jump_Animation");
+            _AudioPlayer.PlayOneShot(JumpingSound);
+                        MyAnim.SetBool("Running",false);
+
+        }
+    
 }
